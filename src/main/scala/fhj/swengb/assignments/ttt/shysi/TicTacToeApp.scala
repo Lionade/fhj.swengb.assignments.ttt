e24f8d7cfccbe8b0d6333c99e773f6940cda7659
Lionade <steven95@gmx.at8rq4ewnIDs4N>
2015-12-13 15:22:40 +0100

    some gui changes; game is working (without bot)

--- src/main/scala/fhj/swengb/assignments/ttt/shysi/TicTacToeApp.scala
+++ src/main/scala/fhj/swengb/assignments/ttt/shysi/TicTacToeApp.scala
@@ -61,21 +61,23 @@ class tttController extends Initializable{
   var t = TicTacToe()
   var outstring: String = ""
 
-  @FXML var L_TopLeft: Button = _
-  @FXML var L_TopCenter: Button = _
-  @FXML var L_TopRight: Button = _
-  @FXML var L_MiddleLeft: Button = _
-  @FXML var L_MiddleCenter: Button = _
-  @FXML var L_MiddleRight: Button = _
-  @FXML var L_BottomLeft: Button = _
-  @FXML var L_BottomCenter: Button = _
-  @FXML var L_BottomRight: Button = _
+  @FXML var B_TopLeft: Button = _
+  @FXML var B_TopCenter: Button = _
+  @FXML var B_TopRight: Button = _
+  @FXML var B_MiddleLeft: Button = _
+  @FXML var B_MiddleCenter: Button = _
+  @FXML var B_MiddleRight: Button = _
+  @FXML var B_BottomLeft: Button = _
+  @FXML var B_BottomCenter: Button = _
+  @FXML var B_BottomRight: Button = _
 
   @FXML var L_Player: Label = _
   @FXML var P_DebugPane: ScrollPane = _
   @FXML var T_DebugArea: TextArea = _
+  @FXML var L_GameOver: Label = _
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
+    L_Player.setText("PlayerA")
   }
 
   //Shows informations about the current game
@@ -86,28 +88,39 @@ class tttController extends Initializable{
   }
 
   def submit(b: Button, move: TMove): Unit ={
-    if(t.remainingMoves.contains(move)){
+    if(t.remainingMoves.contains(move) && !t.gameOver){
       if(t.nextPlayer == PlayerA) b.setText("O") else b.setText("X")
       t = t.turn(move, t.nextPlayer)
       // Check if gameover
+      if(t.gameOver) L_GameOver.setText("Game Over")
       debug()
     }
   }
 
   // Resets the game
   def reset(): Unit ={
-    t = TicTacToe(Map())
-    T_DebugArea.clear()
+    // Noch random machen
+    t = TicTacToe(Map(), PlayerA)
+    T_DebugArea.clear(); outstring = ""
+    clearbuttons()
   }
 
-  @FXML def onTopLeft(): Unit = submit(L_TopLeft, TopLeft)
-  @FXML def onTopCenter(): Unit = submit(L_TopCenter, TopCenter)
-  @FXML def onTopRight(): Unit = submit(L_TopRight, TopRight)
-  @FXML def onMiddleLeft(): Unit = submit(L_MiddleLeft, MiddleLeft)
-  @FXML def onMiddleCenter(): Unit = submit(L_MiddleCenter, MiddleCenter)
-  @FXML def onMiddleRight(): Unit = submit(L_MiddleRight, MiddleRight)
-  @FXML def onBottomLeft(): Unit = submit(L_BottomLeft, BottomLeft)
-  @FXML def onBottomCenter(): Unit = submit(L_BottomCenter, BottomCenter)
-  @FXML def onBottomRight(): Unit = submit(L_BottomRight, BottomRight)
+  def clearbuttons(): Unit = {
+    B_TopLeft.setText(""); B_TopCenter.setText(""); B_TopRight.setText("")
+    B_BottomLeft.setText(""); B_BottomCenter.setText(""); B_BottomRight.setText("")
+    B_MiddleLeft.setText(""); B_MiddleCenter.setText(""); B_MiddleRight.setText("")
+  }
+
+  @FXML def onNewGame(): Unit = {reset(); L_GameOver.setText("")}
+
+  @FXML def onTopLeft(): Unit = submit(B_TopLeft, TopLeft)
+  @FXML def onTopCenter(): Unit = submit(B_TopCenter, TopCenter)
+  @FXML def onTopRight(): Unit = submit(B_TopRight, TopRight)
+  @FXML def onMiddleLeft(): Unit = submit(B_MiddleLeft, MiddleLeft)
+  @FXML def onMiddleCenter(): Unit = submit(B_MiddleCenter, MiddleCenter)
+  @FXML def onMiddleRight(): Unit = submit(B_MiddleRight, MiddleRight)
+  @FXML def onBottomLeft(): Unit = submit(B_BottomLeft, BottomLeft)
+  @FXML def onBottomCenter(): Unit = submit(B_BottomCenter, BottomCenter)
+  @FXML def onBottomRight(): Unit = submit(B_BottomRight, BottomRight)
 
 }
